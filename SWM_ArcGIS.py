8293cd1e3c4faf6d8076c8ed6ccdc24c1e1cd43d
Florian Herz <florian13.herz@googlemail.com>
2019-07-16 16:30:18 +0200

    Aktualisierung des SWM für Arc GIS.

--- SWM_ArcGIS.py
+++ SWM_ArcGIS.py
@@ -32,42 +32,138 @@ __status__ = "Development"
 #  Import der Systemmodule
 import arcpy
 from arcpy.sa import *
-
-#  Einstellungen und Erweiterungen laden
+import time
 
 arcpy.CheckOutExtension("Spatial")
+arcpy.AddMessage(time.strftime("%H:%M:%S: ") + "Systemmodule geladen.")
 
-arcpy.AddMessage("Systemmodule wurden geladen.")
+#  Funktionen
+
+
+def get_pet(haude_factor, temperature, humidity):
+    """Berechnung eines Rasterdatensatzes der PET nach Haude.
+    Es wird der Rasterdatensatz zum  Haude-Faktor mit der Temperatur und der relativen Luftfeuchtigkeit benötigt.
+    """
+    pet_raster = haude_factor * (6.1 * 10 ** ((7.5 * temperature) / (temperature + 237.2))) * (1.0 - humidity / 100.0)
+    return pet_raster
+
+
+def get_aet(pet_raster, water_raster, s_pre_raster, rp_raster, rpwp_dif_raster, wp_raster):
+    """Berechnung der AET je Rasterzelle.
+    Der Wert der AET entspricht bei Gewässerpixeln, sowie Zellen in denen der Bodenwassersgehalt oberhalb des RP liegt
+    der PET. Die AET ist 0 wenn der RP gleich dem WP ist. Bei den anderen Pixeln wird die AET gemäß des SWMs berechnet.
+    """
+    aet_raster = Con(water_raster == 1, pet_raster,
+                     Con(s_pre_raster >= rp_raster, pet_raster,
+                         Con(rpwp_dif_raster == 0, 0, ((s_pre_raster - wp_raster) / rpwp_dif_raster) * pet_raster)))
+    return aet_raster
+
+
+def get_precipitation(timeseries, p_stations_temp, date, idw_pow, rastercellsize):
+    """Niederschlagsinterpolation
+    """
+    station = []
+    p_sum = []
+    with arcpy.da.SearchCursor(timeseries, ["Stationsnummer", "Tagessumme_mm", "TagesID"],
+                               "TagesID = {}".format(date)) as p_cursor:
+        for information in p_cursor:
+            station.append(information[0])
+            p_sum.append(information[1])
+
+    for i in range(len(station)):
+        with arcpy.da.UpdateCursor(p_stations_temp, ["Stationsnummer", "Tagessumme_mm"]) as p_incursor:
+            for p_row in p_incursor:
+                if p_row[0] == station[i]:
+                    p_row[1] = p_sum[i]
+
+                p_incursor.updateRow(p_row)
+
+    idw = Idw(p_stations_temp, "Tagessumme_mm", rastercellsize, idw_pow, RadiusFixed(20000.00000, 5), "")
+
+    # Delete temporary objects
+    del p_cursor
+    del p_incursor
+
+    return idw
+
+
+def overflow(p_raster, s_pre_raster, fk_raster):
+    """Berechnet den direkten Abfluss des Niederschlags bei gesättigtem Bodenwasser
+    """
+    r_overflow = Con(p_raster + s_pre_raster > fk_raster,
+                     p_raster + s_pre - fk_raster, 0)
+    return r_overflow
+
+
+def runoff_land(water_raster, lambda_wert, wp_raster, p_raster, s_pre_raster, fk_raster):
+    """Berechnet den Abfluss von Landpixeln
+    """
+    r_land = Con(water_raster == 0,
+                 lambda_wert * ((s_pre - wp_raster) ** 2) + overflow(p_raster, s_pre_raster, fk_raster), 0)
+    return r_land
+
+
+def get_runoff(water_raster, lambda_wert, wp_raster, p_raster, s_pre_raster, fk_raster, pet_raster):
+    """Berechnung des Oberflächenabflusses
+    Niederschlag_id_day, s_pre, fk, water, lambda, wp, pet
+    """
+    r = runoff_land(water_raster, lambda_wert, wp_raster, p_raster, s_pre_raster, fk_raster) +\
+        water_raster * (p_raster - pet_raster)
+    return r
+
+
+def get_soilwater(s_pre_raster, p_raster, aet_raster, runoff_raster):
+    """Berechnung des aktuellen Bodenwasserspeichers
+    """
+    soilwater = s_pre_raster + p_raster - aet_raster - runoff_raster
+    return soilwater
+
 
 #  Benutzereingaben
 
 data = arcpy.GetParameterAsText(0)  # Datatype: workspace #  Default: C:\HiWi_Hydro-GIS\MTP_HydroGIS_Basisdaten.gdb
 
-name = arcpy.GetParameterAsText(1)  # Datatype: string #  Default: SWM_Ergebnisdatenbank_2003-2004_I.gdb
+basin = arcpy.GetParameterAsText(1)  # Datatype: Featuredataset
+#  Default: C:\HiWi_Hydro-GIS\MTP_HydroGIS_Basisdaten.gdb\EZG_Schotten2_Vektor
+basin_id = arcpy.GetParameterAsText(2)  # String #  Default: Id
 
-folder = arcpy.GetParameterAsText(2)  # Datatype: folder #  Default: C:\HiWi_Hydro-GIS
+name = arcpy.GetParameterAsText(3)  # Datatype: string #  Default: SWM_Ergebnisdatenbank_2003-2004_I.gdb
 
-start = int(arcpy.GetParameterAsText(3))  # Datatype: long #  Default: 20030101
+folder = arcpy.GetParameterAsText(4)  # Datatype: folder #  Default: C:\HiWi_Hydro-GIS
 
-end = int(arcpy.GetParameterAsText(4))  # Datatype: long #  Default: 20041231
+start = int(arcpy.GetParameterAsText(5))  # Datatype: long #  Default: 20030101
 
-s_init = Raster(arcpy.GetParameterAsText(5))  # Datatype: geodataset
+end = int(arcpy.GetParameterAsText(6))  # Datatype: long #  Default: 20041231
+
+s_init = Raster(arcpy.GetParameterAsText(7))  # Datatype: geodataset
 #  Default: C:\HiWi_Hydro-GIS\MTP_HydroGIS_Basisdaten.gdb\FK_von_L
 
-rp_factor = float(arcpy.GetParameterAsText(6))  # Datatype: double #  Default: 0.85
+rp_factor = float(arcpy.GetParameterAsText(8))  # Datatype: double #  Default: 0.85
+
+c = int(arcpy.GetParameterAsText(9))  # Datatype: short #  Default: 150
+
+idw_exponent = arcpy.GetParameterAsText(10)  # Datatype: string #  Default: 1
 
 #  Abfrage welche Daten gespeichert werden sollen
-check_pet = str(arcpy.GetParameterAsText(7))  # Datatype: boolean #  Default: false
+check_pet = str(arcpy.GetParameterAsText(11))  # Datatype: boolean #  Default: false
 
-check_aet = str(arcpy.GetParameterAsText(8))  # Datatype: boolean #  Default: false
+check_aet = str(arcpy.GetParameterAsText(12))  # Datatype: boolean #  Default: false
+
+check_p = str(arcpy.GetParameterAsText(13))  # Datatype: boolean #  Default: false
+
+check_r = str(arcpy.GetParameterAsText(14))  # Datatype: boolean #  Default: false
+
+check_s = str(arcpy.GetParameterAsText(15))  # Datatype: boolean #  Default: false
 
 #  Erstellen der Ergebnisdatenbank und Wahl des Arbeitsverzeichnisses
 
+arcpy.env.overwriteOutput = True
+arcpy.env.extent = s_init
 arcpy.CreateFileGDB_management(folder, name)
 arcpy.env.workspace = r'{0}\{1}'.format(folder, name)
 arcpy.env.scratchWorkspace = arcpy.env.workspace
 
-arcpy.AddMessage("Die Ergebnisdatenbank wurde im Verzeichnis {} erstellt.".format(folder))
+arcpy.AddMessage(time.strftime("%H:%M:%S: ") + "Die Ergebnisdatenbank wurde im Verzeichnis {} erstellt.".format(folder))
 
 #  Zuweisung der Rasterdatensätze
 
@@ -88,11 +184,19 @@ climatedata = r'{}\TempFeuchte'.format(data)  # Grundlegende Klimadatentabelle
 fk = Raster(r'{}\fk_von_L'.format(data))
 rp = fk * rp_factor
 wp = Raster(r'{}\wp'.format(data))
-gewaesser = Raster(r'{}\Gewaesser'.format(data))
+water = Raster(r'{}\Gewaesser'.format(data))
 rpwp_dif = rp - wp
 s_pre = s_init
+p_data = r'{}\N_Zeitreihen'.format(data)
+p_temp = arcpy.Copy_management(r'{}\N_Messstationen'.format(data), "p_temp")
+arcpy.AddField_management(p_temp, "Tagessumme_mm", "DOUBLE")
+cellsize = arcpy.Describe(s_init).children[0].meanCellHeight
+lambda_parameter = (c / (Raster(r'{}\L_in_metern'.format(data)) * 1000)**2)
+runoff_daily = []
+id_list = []
+date_list = []
 
-arcpy.AddMessage("Berechnung der Rasterdatensätze war erfolgreich.")
+arcpy.AddMessage(time.strftime("%H:%M:%S: ") + "Berechnung der Rasterdatensaetze war erfolgreich.")
 
 #  Iteration durch die ausgewählten Klimadaten
 
@@ -109,22 +213,67 @@ with arcpy.da.SearchCursor(climatedata, ['Tagesid', 'Jahr', 'Monat', 'Tag', 'Rel
 
         # Berechnung der PET
 
-        es = 6.1 * 10**((7.5 * temp) / (temp + 237.2))
-        pet = haude_dic[month] * es * (1.0 - humid / 100.0)  # Berechnung der PET
+        pet = get_pet(haude_dic[month], temp, humid)
 
-        #  Speichern der täglichen PET Rasterdateien
-        if check_pet == 'true':
-            pet.save("PET_{}".format(id_day))
-        
         # Berechnung der AET
 
-        aet = Con(gewaesser == 1, pet, Con(s_pre >= rp, pet, Con(rpwp_dif == 0, 0, ((s_pre - wp)/rpwp_dif)*pet)))
+        aet = get_aet(pet, water, s_pre, rp, rpwp_dif, wp)
 
-        # Speichern der täglichen AET Rasterdateien
+        #  Niederschlagsinterpolation
+
+        precipitation = get_precipitation(p_data, p_temp, id_day, idw_exponent, cellsize)
+
+        #  Ablfussberechnung
+
+        runoff = get_runoff(water, lambda_parameter, wp, precipitation, s_pre, fk, pet)
+
+        #  Berechnung des Bodenwasserspeichers
+
+        s = get_soilwater(s_pre, precipitation, aet, runoff)
+
+        #  Speichern der täglichen Rasterdateien
+        if check_pet == 'true':
+            pet.save("PET_{}".format(id_day))
+
         if check_aet == 'true':
             aet.save("AET_{}".format(id_day))
 
-        arcpy.AddMessage("Fertig mit der Berechnung des {0}.{1}.{2}".format(day, month, year))
-        
-# Delete cursor objects
+        if check_p == 'true':
+            precipitation.save("Niederschlag_{}".format(id_day))
+
+        if check_r == 'true':
+            runoff.save("R_{}".format(id_day))
+
+        if check_s == 'true':
+            s.save("S_{}".format(id_day))
+
+        s_pre = s
+        ZonalStatisticsAsTable(basin, basin_id, runoff, "Q_day", "true", "SUM")
+        with arcpy.da.SearchCursor("Q_day", "Sum") as r_cursor:
+            for r_sum in r_cursor:
+                runoff_daily.append(r_sum[0] * 0.001 * cellsize ** 2)
+        id_list.append(id_day)
+        date_list.append("{0}.{1}.{2}".format(day, month, year))
+
+        arcpy.AddMessage(time.strftime("%H:%M:%S: ") +
+                         "Fertig mit der Berechnung des {0}.{1}.{2}".format(day, month, year))
+
+arcpy.CreateTable_management(arcpy.env.workspace, "Durchfluss")
+arcpy.AddField_management("Durchfluss", "Datum", "STRING")
+arcpy.AddField_management("Durchfluss", "Q", "DOUBLE")
+arcpy.AddField_management("Durchfluss", "Tages_ID", "LONG")
+
+q_cursor = arcpy.da.InsertCursor("Durchfluss", ["Datum", "Q", "Tages_ID"])
+
+for i in range(len(id_list)):
+    row = [date_list[i], runoff_daily[i], id_list[i]]
+    q_cursor.insertRow(row)
+
+# Delete temporary objects
 del cursor
+del q_cursor
+del row
+arcpy.Delete_management("Q_day")
+arcpy.Delete_management("p_temp")
+
+arcpy.AddMessage(time.strftime("%H:%M:%S: ") + "Modellierung Abgeschlossen.")
